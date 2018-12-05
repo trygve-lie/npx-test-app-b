3ac7865b872d52668f54721bc568004603cd5e04
Trygve Lie <post@trygve-lie.com>
2018-12-05 22:12:53 +0100

    Added app

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+'use strict';
+
+const app = require('express')();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from app-b');
+});
+
+app.listen(5000, () => {
+    console.log('app-b running at http://localhost:5000');
+});
