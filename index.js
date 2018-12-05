eddb5bf16dab0148506adf5f139d4518441c0683
Trygve Lie <post@trygve-lie.com>
2018-12-05 22:34:19 +0100

    Print node.js version

--- index.js
+++ index.js
@@ -9,4 +9,5 @@ app.get('/', (req, res) => {
 
 app.listen(5000, () => {
     console.log('app-b running at http://localhost:5000');
+    console.log('node.js version used', process.versions.node);
 });
